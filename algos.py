479c74bdde1b497ea4eac7a4720f8af6fe5e1d2d
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-01-05 23:24:14 -0700

    add second derivative

--- algos.py
+++ algos.py
@@ -5,3 +5,7 @@ def smooth_price(price, smoothness):
     box = np.ones(smoothness)/smoothness
     price_smooth = np.convolve(price, box, mode='same')
     return price_smooth
+
+
+def second_derivative(curve):
+    return curve.derivative(n=2)
